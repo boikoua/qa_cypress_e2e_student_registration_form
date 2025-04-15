5f6bc75ca00efbb0f6ed6203a131a69c660aaa1f
Dmytro Boiko <iamboikodmytro@gmail.com>
2025-04-15 09:36:01 +0300

    fixed the task

--- cypress/e2e/registration.cy.js
+++ cypress/e2e/registration.cy.js
@@ -1,20 +1,27 @@
 /// <reference types='cypress' />
 
+const username = 'John';
+const surname = 'Johnson';
+const email = 'johnjohnson@gmail.com';
+const gender = 'Male';
+const mobile = '5345985433';
+const address = '6200 Winner street, New-York, USA';
+
 describe('Student Registration page', () => {
   before(() => {
     cy.visit('/');
   });
 
   it('should be success registration', () => {
-    cy.get('#firstName').type('John');
+    cy.get('#firstName').type(username);
 
-    cy.get('#lastName').type('Johnson');
+    cy.get('#lastName').type(surname);
 
-    cy.get('#userEmail').type('johnjohnson@gmail.com');
+    cy.get('#userEmail').type(email);
 
-    cy.get('.custom-control-label').contains('Male').click();
+    cy.get('.custom-control-label').contains(gender).click();
 
-    cy.get('#userNumber').type('53459854335');
+    cy.get('#userNumber').type(mobile);
 
     cy.get('#dateOfBirthInput').click();
 
@@ -24,7 +31,7 @@ describe('Student Registration page', () => {
 
     cy.get('.custom-control-label').contains('Sports').click();
 
-    cy.get('#currentAddress').type('6200 Winner street, New-York, USA');
+    cy.get('#currentAddress').type(address);
 
     cy.get('#state').click();
     cy.contains('div', 'NCR').click();
@@ -37,6 +44,53 @@ describe('Student Registration page', () => {
     cy.get('.modal-title').should(
       'contain.text',
       'Thanks for submitting the form'
+    );
+
+    cy.get('tbody > :nth-child(1) > :nth-child(2)').should(
+      'contain.text',
+      `${username} ${surname}`
+    );
+
+    cy.get('tbody > :nth-child(2) > :nth-child(2)').should(
+      'contain.text',
+      email
+    );
+
+    cy.get('tbody > :nth-child(3) > :nth-child(2)').should(
+      'contain.text',
+      gender
+    );
+
+    cy.get('tbody > :nth-child(4) > :nth-child(2)').should(
+      'contain.text',
+      mobile
+    );
+
+    cy.get('tbody > :nth-child(5) > :nth-child(2)').should(
+      'contain.text',
+      '01 April,2025'
+    );
+
+    cy.get('tbody > :nth-child(6) > :nth-child(2)').should(
+      'contain.text',
+      'Maths'
+    );
+
+    cy.get('tbody > :nth-child(7) > :nth-child(2)').should(
+      'contain.text',
+      'Sports'
+    );
+
+    cy.get('tbody > :nth-child(8) > :nth-child(2)').should('contain.text', '');
+
+    cy.get('tbody > :nth-child(9) > :nth-child(2)').should(
+      'contain.text',
+      address
+    );
+
+    cy.get('tbody > :nth-child(10) > :nth-child(2)').should(
+      'contain.text',
+      'NCR Delhi'
     );
   });
 });
